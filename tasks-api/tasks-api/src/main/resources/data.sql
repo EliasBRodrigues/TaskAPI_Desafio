b1bf82f4e079216d83f7286e07df9b9183c22616
EliasBRodrigues <elias.rborges3@gmail.com>
2024-06-27 22:18:22 -0300

    add: properties database

---
+++ tasks-api/tasks-api/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO task(title_task, description_task, completed_task) VALUES
+('STUDY SPRING', 'SPRING BOOT 3', FALSE);
+
+INSERT INTO task(title_task, description_task, completed_task) VALUES
+('STUDY JAVA', 'SPRING JAVA 3', TRUE);
